401a6734e76bfa2873f9c0f4bc309013426d0df8
Antonio King <antonio.j.king@gmail.com>
2021-09-26 15:31:47 -0600

    Complete weather facade

---
+++ spec/facades/weather_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe WeatherFacade do
+  describe 'class methods', :vcr do
+    let(:location) { MapquestFacade.coordinates_by_city_state('denver,co') }
+
+    it '::forecast_by_coordinates' do
+      forecast = WeatherFacade.forecast_by_coordinates(location)
+      expect(forecast).to be_a(Forecast)
+    end
+  end
+end
