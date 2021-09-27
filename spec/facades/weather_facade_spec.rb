ec8200da01ddc7d87fa8016f43a1d59cece56c0d
Antonio King <antonio.j.king@gmail.com>
2021-09-27 13:57:27 -0600

    fix vcr and serializer file structure

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe WeatherFacade do
   describe 'class methods', :vcr do
-    let(:location) { MapquestFacade.coordinates_by_city_state('denver,co') }
+    let(:location) { 'denver,co' }
 
     it '::forecast_by_coordinates' do
       forecast = WeatherFacade.forecast_by_city_state(location)
